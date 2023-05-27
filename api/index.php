aff087dea968ac393579ba2a42b04f6c65890829
Scott Henshaw <scott.henshaw86@gmail.com>
2023-05-27 14:27:33 +0900

    almost done

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+
+if (empty($_GET['api_key']) && $_GET['api_key'] !== getenv("API_KEY")) {
+    http_response_code(401);
+    exit;
+}
+
+if (empty($_GET['city'])) {
+    echo "missing required parameter: city";
+    http_response_code(400);
+    exit;
+}
+
+if (empty($_GET['state'])) {
+    echo "missing required parameter: state";
+    http_response_code(400);
+    exit;
+}
+
+$city = strtolower(str_replace("+", "_", $_GET['city']));
+$state = strtolower($_GET['state']);
+$filename = "$city-$state.json";
+
+$data = file_get_contents(__DIR__ . '/../data/' . $filename);
+
+echo $data;
