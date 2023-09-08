092ef33501d1272c85a6f2dde3bd38bc937ef797
Scott Henshaw <scotthenshaw86@gmail.com>
2023-09-08 09:56:24 +0900

    update cors

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
- header("Access-Control-Allow-Origin: http://localhost");
+ header("Access-Control-Allow-Origin: *");
 
 if (empty($_GET['api_key']) or $_GET['api_key'] !== getenv("API_KEY")) {
     http_response_code(401);
